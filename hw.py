67db5ded3c18504fcd3faee7ff0e853478ab28ec
chengy.yao001 <chengy.yao001@bkjk.com>
2021-03-13 11:09:47 +0800

    添加文件

---
+++ hw.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/3/12 9:24
+# @Author  : ycy
+# @Email   : chengy.yao001@bkjk.com
+# @File    : hw.py
+# @Software: PyCharm
+print("hello world")
